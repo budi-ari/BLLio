7986be93b23161f21c495613ca9e9f906c9e80fd
mexop99 <mexop99@gmail.com>
2019-12-04 13:12:03 +0700

    update UI baru
    muk dandani ui e tok, ben rdok ayu sitik wkwkwkwkw

--- index.php
+++ index.php
@@ -1,74 +1,68 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en" id="beranda">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BLLio</title>
+
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style/style.css">
     <link rel="stylesheet" type="text/css" href="produk/produk.html">
-
-    <title>BLLio</title>
 </head>
 
 <body>
-	<?php
-		include "navbar.php";
-	?>
-    <!-- pembagian kolom -->
-    <div class="row mt-5 no-gutters">
-        <div class="col-md-2 bg-light" style="background-color: bisque;">
-            <div class="list-group list-group-flush p-1 pt-3">
-                <a href="#" class="list-group-item list-group-item-action active font-weight-bold"> <i class="fas fa-list mr-2"></i>KATEGORI PRODUK</a>
-                <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-angle-right"></i> Buku</a>
-                <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-angle-right"></i> IT/Komputer</a>
-                <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-angle-right"></i> Alat Peraga Pendidikan</a>
-            </div>
-        </div>
-        <div class="col-md-10">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/carousel/carousel1.jpg" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/carousel/carousel2.jpg" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/carousel/carousel3.jpg" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/carousel/carousel4.jpg" class="d-block w-100">
-                    </div>
+    <?php
+    include "navbar.php";
+    ?>
+
+    <div class="container" id="contenthome">
+        <div id="carouselHome" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ul class="carousel-indicators">
+                <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselHome" data-slide-to="1"></li>
+                <li data-target="#carouselHome" data-slide-to="2"></li>
+            </ul>
+            <!-- The slideshow -->
+            <div class="carousel-inner" id="myslide">
+                <div class="carousel-item active">
+                    <img src="img/carousel/carousel1.jpg" alt="slide1" class="img-fluid rounded mx-auto d-block">
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item">
+                    <img src="img/carousel/carousel2.jpg" alt="slide2" class="img-fluid rounded mx-auto d-block">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/carousel/carousel3.jpg" alt="slide3" class="img-fluid rounded mx-auto d-block">
+                </div>
             </div>
-
-            <?php
-            include "produk.php";
-            ?>
-
-            <?php
-            include "popup.php";
-            ?>            
-
+            <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#carouselHome" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselHome" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
         </div>
+
+        <?php
+        include "produk.php";
+        ?>
+
+        <?php
+        include "popup.php";
+        ?>
     </div>
-    <!-- akhir pembagian kolom -->
+
+
+
 
     <!-- footer -->
-    <footer class="bg-dark text-white ">
-        <div class="copyright text-center font-weight-bold bg-dark text-white p-2 mt-3 ">
+    <footer>
+        <div class="copyright text-center font-weight-bold text-white p-2 mt-3 ">
             <p class="mt-3 "><i class="far fa-copyright "></i> copyright 2019 | Pemrograman Web</p>
         </div>
     </footer>
@@ -81,7 +75,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js " integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1 " crossorigin="anonymous "></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js " integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM " crossorigin="anonymous "></script>
     <script src="https://kit.fontawesome.com/85a2f234d7.js " crossorigin="anonymous "></script>
-
 </body>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -1,36 +1,36 @@
    <!-- navbar -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color:lightgray">
-        <div class="container">
-            <h3><i class="fas fa-shopping-bag text-dark mr-2"></i></h3>
-            <a class="navbar-brand font-weight-bold" href="#beranda">BLLio</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+   <nav class="navbar fixed-top navbar-expand-lg navbar-light">
+       <div class="container">
+           <h3><i class="fas fa-shopping-bag text-dark mr-2"></i></h3>
+           <a class="navbar-brand font-weight-bold" href="#beranda">BLLio</a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto mr-4">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#produkKami">Produk <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Promo <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Bantuan <span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-                <div class="icon mt-2">
-                    <h5>
-                        <i class="fas fa-shopping-cart text-dark ml-3 mr-3" href="#" data-toggle="tootip" title="Keranjang"></i>
-                        <i class="fas fa-envelope text-dark mr-3" href="#" data-toggle="tootip" title="Kotak Masuk"></i>
-                        <i class="fas fa-bell text-dark mr-3" href="#" data-toggle="tootip" title="Notifikasi"></i>
-                    </h5>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <!-- akhir navbar -->
+           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               <ul class="navbar-nav ml-auto mr-4">
+                   <li class="nav-item">
+                       <a class="nav-link" href="#produkKami">Produk <span class="sr-only">(current)</span></a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="#">Promo <span class="sr-only">(current)</span></a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="#">Bantuan <span class="sr-only">(current)</span></a>
+                   </li>
+               </ul>
+               <form class="form-inline my-2 my-lg-0">
+                   <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                   <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+               </form>
+               <div class="icon mt-2">
+                   <h5>
+                       <i class="fas fa-shopping-cart text-white ml-3 mr-3" href="#" data-toggle="tootip" title="Keranjang"></i>
+                       <i class="fas fa-envelope text-white mr-3" href="#" data-toggle="tootip" title="Kotak Masuk"></i>
+                       <i class="fas fa-bell text-white mr-3" href="#" data-toggle="tootip" title="Notifikasi"></i>
+                   </h5>
+               </div>
+           </div>
+       </div>
+   </nav>
+   <!-- akhir navbar -->
