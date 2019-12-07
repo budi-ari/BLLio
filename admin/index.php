f4574f2f6788ea9f0ae53a5a01fc676cdb47ef3d
budi-ari <budi.ari@gmail.com>
2019-12-07 12:36:59 +0700

    belum kelar, bentar mau keluar

--- admin/index.php
+++ admin/index.php
@@ -3,8 +3,11 @@
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BLLio</title>
+    <link rel="shortcut icon" href="../img/favicon-32x32.png" type="image/png">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include "db/config.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LLio</title>
+    <title>BLLio</title>
     <link rel="shortcut icon" href="img/favicon-32x32.png" type="image/png">
     <!-- <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"> -->
 
