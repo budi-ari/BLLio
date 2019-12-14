695b11b43c1eae97e199938b451f8f3f4724b25c
budi-ari <budi.ari@gmail.com>
2019-12-14 20:07:39 +0700

    crud carousel done

--- admin/DB_admin/DBUpdate.php
+++ admin/DB_admin/DBUpdate.php
@@ -1,9 +1,9 @@
 <?php 
 include "../login/config.php";
 
-if(isset($_POST['nama'])){
-$ubah = mysqli_query($con, "Update admin set nama = '$_POST[nama]', username = '$_POST[username]', password = '$_POST[password]'
-                            where nama = '$_POST[nama]'");
+if(isset($_POST['id'])){
+$ubah = mysqli_query($con, "Update admin set id = '$_POST[id]', nama = '$_POST[nama]', username = '$_POST[username]', password = '$_POST[password]'
+                            where id = '$_POST[id]'");
 
     if($ubah){
         header('Location: ../halaman_admin.php');
