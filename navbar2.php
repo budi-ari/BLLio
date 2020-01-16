157e052dc4dfa6c498a11d5603ae70ebe55a31c5
budi-ari <budi.ari@gmail.com>
2020-01-16 19:12:33 +0700

    pembaruan 4

---
+++ navbar2.php
@@ -0,0 +1,36 @@
+   <!-- navbar -->
+   <nav class="navbar fixed-top navbar-expand-lg navbar-light">
+       <div class="container">
+           <a href="#beranda">
+               <img src="img/icon/bllio.png" alt="logo-bllio" style="width:100px;">
+           </a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+               <ul class="navbar-nav ml-auto mr-4">
+                   <li class="nav-item">
+                       <a class="nav-link" href="admin/halaman_admin.php">Kembali Ke Dashboard<span class="sr-only">(current)</span></a>
+                   </li>
+                   <!-- <li class="nav-item">
+                       <a class="nav-link" href="#produkKami">Produk <span class="sr-only">(current)</span></a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="#">Bantuan <span class="sr-only">(current)</span></a>
+                   </li> -->
+               </ul>
+               <!-- <form class="form-inline my-2 my-lg-0">
+                   <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                   <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+               </form> -->
+               <!-- <div class="icon mt-2">
+                   <h5>
+                       <i class="fas fa-shopping-cart text-white ml-3 mr-3" href="#" data-toggle="tootip" title="Keranjang"></i>
+                       <i class="fas fa-envelope text-white mr-3" href="#" data-toggle="tootip" title="Kotak Masuk"></i>
+                       <i class="fas fa-bell text-white mr-3" href="#" data-toggle="tootip" title="Notifikasi"></i>
+                   </h5>
+               </div> -->
+           </div>
+       </div>
+   </nav>
+   <!-- akhir navbar -->
